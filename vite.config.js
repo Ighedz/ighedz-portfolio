a025c9104b8dc2d139642dd782d3f4b43335a339
Ighedz <ighedosapromisei@gmail.com>
2025-07-20 08:35:16 +0100

    Fix Vite base config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/ighedz-portfolio'
+  plugins: [react()],
+  base: '/', // this is fine for Netlify
 })
+
